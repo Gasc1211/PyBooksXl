df6499b3bdf2e7886699a7ffb663453cf963e518
Gasc1211 <gabriel.suazo1211@gmail.com>
2020-08-15 10:45:56 -0600

    create workbook and format it

--- init.py
+++ init.py
@@ -11,4 +11,19 @@ dirName += f'\{date.year}'
 os.makedirs(dirName)
 os.chdir(dirName)
 
+workbook = Workbook()
+workbook.remove_sheet(workbook.active)
 
+sheets = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
+          'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+
+for sheet in sheets:
+    activeSheet = workbook.create_sheet(sheet)
+    activeSheet['A1'] = "Fecha"
+    activeSheet['B1'] = "Factura"
+    activeSheet['C1'] = "Contado"
+    activeSheet['D1'] = "Credito"
+    activeSheet['E1'] = "Impuesto"
+    activeSheet['F1'] = "Total"
+
+workbook.save(filename="sales.xlsx")
